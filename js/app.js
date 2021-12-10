433c62df3455e551a3858eaf6b3fba6486c62123
Diego Maldonado <dj_maldonado19@hotmail.es>
2021-12-09 22:43:32 -0600

    Subiendo archivos

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let login = ()=>{
+    const getUsernameDiv = document.getElementById('usernameDiv');
+    const getPasswordDiv = document.getElementById('passwordDiv');
+
+    console.log(getUsernameDiv);
+    console.log(getPasswordDiv);
+
+    console.log('aqui deberia de ver el username y password')
+
+}
+
+let registrar = ()=>{
+
+    console.log('datos para ingresar a un usuario');
+}
